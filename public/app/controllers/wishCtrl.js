c61b10916746b77853b0d78caa8d42377c889d96
Abhinav Gupta <abhinav3006gupta@gmail.com>
2017-09-28 15:39:38 +0530

    ios whatsapp

--- public/app/controllers/wishCtrl.js
+++ public/app/controllers/wishCtrl.js
@@ -16,7 +16,7 @@ angular.module('wishController',['wishService'])
                 if (data.data.success) {
                     app.successmsg = "Your wish has been created now you can share it.";
                     app.pub_url = "wish4you.ga/"+data.data.choice+"?counter="+data.data.counter;
-                    app.whatsapp_url = "👌 An app to send wish to your dear ones in a unique way. 😍 Wish4you.ga/"+data.data.choice+"?counter="+data.data.counter;
+                    app.whatsapp_url = "👌 An app to send wish to your dear ones in a unique way. 😍 http://Wish4you.ga/"+data.data.choice+"?counter="+data.data.counter;
                 }
                 else {
 
